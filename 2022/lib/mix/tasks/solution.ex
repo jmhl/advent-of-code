159f36d225d77087a9f44530d91fce738ab169f1
James Haley <james.haley@neednova.com>
2022-12-03 10:38:17 -0800

    Create single task to run solutions

---
+++ 2022/lib/mix/tasks/solution.ex
@@ -0,0 +1,12 @@
+defmodule Mix.Tasks.Solution do
+  use Mix.Task
+
+  def run([day_num, part_num]) do
+    module_name = Module.concat(AdventOfCode2022, "Day#{day_num}")
+    method_name = String.to_atom("part#{part_num}")
+
+    result = apply(module_name, method_name, [])
+
+    IO.puts "Result for day #{day_num}, part #{part_num}:\n\n#{result}"
+  end
+end
