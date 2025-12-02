e947d4d09f1f0b1d6c1f1389a9b75473e6fd138d
jameshaley <jlhaley1@gmail.com>
2025-12-01 20:10:39 -0800

    Add day 2 part 2 solution

--- 2025/src/day1/day1.lua
+++ 2025/src/day1/day1.lua
@@ -6,6 +6,9 @@ local occurrences_of_zero = 0
 for line in file:lines() do
 	local dir, num_s = string.match(line, "(%w)(%d+)")
 	local num = tonumber(num_s) % 100
+	local passed_zero = tonumber(num_s) // 100
+
+	occurrences_of_zero = occurrences_of_zero + passed_zero
 
 	local intermediate_value
 
@@ -15,18 +18,27 @@ for line in file:lines() do
 		intermediate_value = position + num
 	end
 
-	if intermediate_value > 99 then
+	if intermediate_value == 0 then
+		position = intermediate_value
+		occurrences_of_zero = occurrences_of_zero + 1
+	elseif intermediate_value > 99 then
 		local difference = intermediate_value - 99 - 1
+
+		if not(position == 0) then
+			occurrences_of_zero = occurrences_of_zero + 1
+		end
+
 		position = difference
 	elseif intermediate_value < 0 then
 		local difference = 99 + intermediate_value + 1
+
+		if not(position == 0) then
+			occurrences_of_zero = occurrences_of_zero + 1
+		end
+
 		position = difference
 	else
 		position = intermediate_value
-	end
-
-	if position == 0 then
-		occurrences_of_zero = occurrences_of_zero + 1
 	end
 
 	print("Command: " .. line .. " | Position: " .. position)
