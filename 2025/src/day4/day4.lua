9e2a027598942a5477d60d2a25d93eb5994b9e41
jameshaley <jlhaley1@gmail.com>
2025-12-04 20:44:25 -0800

    Add day 4 part 2 solution + gitignore inputs

--- 2025/src/day4/day4.lua
+++ 2025/src/day4/day4.lua
@@ -23,52 +23,71 @@ function build_grid()
 	return grid
 end
 
-local grid = build_grid()
+function remove_rolls(grid)
+	local marked_for_removal = {}
 
-for row, row_val in ipairs(grid) do
-	for col, col_val in ipairs(row_val) do
-		local can_forklift = true
+	for row, row_val in ipairs(grid) do
+		for col, col_val in ipairs(row_val) do
+			local can_forklift = true
 
-		if col_val == "@" then
-			local adjacent_roll_count = 0;
+			if col_val == "@" then
+				local adjacent_roll_count = 0;
 
-			if row_val[col - 1] == "@" then
-				adjacent_roll_count = adjacent_roll_count + 1
-			end
-			if row_val[col + 1] == "@" then
-				adjacent_roll_count = adjacent_roll_count + 1
-			end
-			if (grid[row - 1] and grid[row - 1][col] == "@") then
-				adjacent_roll_count = adjacent_roll_count + 1
-			end
-			if (grid[row + 1] and grid[row + 1][col] == "@") then
-				adjacent_roll_count = adjacent_roll_count + 1
-			end
-			if (grid[row - 1] and grid[row - 1][col - 1] == "@") then
-				adjacent_roll_count = adjacent_roll_count + 1
-			end
-			if (grid[row - 1] and grid[row - 1][col + 1] == "@") then
-				adjacent_roll_count = adjacent_roll_count + 1
-			end
-			if (grid[row + 1] and grid[row + 1][col - 1] == "@") then
-				adjacent_roll_count = adjacent_roll_count + 1
-			end
-			if (grid[row + 1] and grid[row + 1][col + 1] == "@") then
-				adjacent_roll_count = adjacent_roll_count + 1
-			end
+				if row_val[col - 1] == "@" then
+					adjacent_roll_count = adjacent_roll_count + 1
+				end
+				if row_val[col + 1] == "@" then
+					adjacent_roll_count = adjacent_roll_count + 1
+				end
+				if (grid[row - 1] and grid[row - 1][col] == "@") then
+					adjacent_roll_count = adjacent_roll_count + 1
+				end
+				if (grid[row + 1] and grid[row + 1][col] == "@") then
+					adjacent_roll_count = adjacent_roll_count + 1
+				end
+				if (grid[row - 1] and grid[row - 1][col - 1] == "@") then
+					adjacent_roll_count = adjacent_roll_count + 1
+				end
+				if (grid[row - 1] and grid[row - 1][col + 1] == "@") then
+					adjacent_roll_count = adjacent_roll_count + 1
+				end
+				if (grid[row + 1] and grid[row + 1][col - 1] == "@") then
+					adjacent_roll_count = adjacent_roll_count + 1
+				end
+				if (grid[row + 1] and grid[row + 1][col + 1] == "@") then
+					adjacent_roll_count = adjacent_roll_count + 1
+				end
 
-			if adjacent_roll_count >= 4 then
+				if adjacent_roll_count >= 4 then
+					can_forklift = false
+				end
+			else
 				can_forklift = false
 			end
-		else
-			can_forklift = false
-		end
 
-		if can_forklift then
-			total = total + 1
+			if can_forklift then
+				total = total + 1
+
+				table.insert(marked_for_removal, {row, col})
+			end
 		end
 	end
+
+	for k, v in pairs(marked_for_removal) do
+		row = v[1]
+		col = v[2]
+
+		grid[row][col] = "."
+	end
+
+	if #marked_for_removal > 0 then
+		remove_rolls(grid)
+	end
 end
+
+local grid = build_grid()
+
+remove_rolls(grid)
 
 print(total)
 
