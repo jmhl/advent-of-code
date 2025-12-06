01d2db612ba9efe39ecb266512abe3a730b35a35
jameshaley <jlhaley1@gmail.com>
2025-12-06 09:57:19 -0800

    Update day 5 for part 2 - solution wrong

--- 2025/src/day5/day5.lua
+++ 2025/src/day5/day5.lua
@@ -1,34 +1,62 @@
 file = io.open("./src/day5/input.txt", "r")
 
-total = 0
-
 local fresh_ingredients = {}
+local unique_ranges = {}
 
 for line in file:lines() do
 	if string.find(line, "-") then
 		local low_s, high_s = string.match(line, "(%d+)-(%d+)")
-
 		local low = tonumber(low_s)
 		local high = tonumber(high_s)
 
 		table.insert(fresh_ingredients, { low, high })
-	elseif line ~= "" then
-		local num = tonumber(line)
+	end
+end
 
-		local is_fresh = false;
+table.sort(fresh_ingredients, function(i1, i2)
+	local l1, h1 = table.unpack(i1)
+	local l2, h2 = table.unpack(i2)
 
-		for idx, range in pairs(fresh_ingredients) do
-			local low, high = table.unpack(range)
+	return l1 < l2
+end)
 
-			if num >= low and num <= high then
-				is_fresh = true
+function is_in_range(val, low, high)
+	return val >= low and val <= high
+end
+
+for idx, ingredients in pairs(fresh_ingredients) do
+	local low, high = table.unpack(ingredients)
+	local is_new_range = true
+
+	for idx, range in pairs(unique_ranges) do
+		local ex_low, ex_high = table.unpack(range)
+
+		if is_in_range(ex_low, low, high) or is_in_range(ex_high, low, high) then
+			is_new_range = false
+
+			if ex_low < low then
+				unique_ranges[idx] = { ex_low, high }
+			end
+
+			if ex_high > high then
+				unique_ranges[idx] = { low, ex_high }
 			end
 		end
-
-		if is_fresh then
-			total = total + 1
-		end
 	end
+
+	if is_new_range then
+		table.insert(unique_ranges, { low, high })
+	end
+end
+
+total = 0
+
+for idx, range in pairs(unique_ranges) do
+	local low, high = table.unpack(range)
+	local diff = high - low + 1
+
+	total = total + diff
+	-- print(low, high, diff)
 end
 
 print(total)
